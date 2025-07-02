97481164fbadf298ca117701aadc34f04d13f27c
Oleh <berolegnik@gmail.com>
2025-07-02 23:08:48 +0300

    active

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-	$('.header__nav-btn').click(function(){
-		$(this).toggleClass('active');
-	});
+$(document).ready(function () {
+  $(".header__nav-btn").click(function () {
+    $(this).toggleClass("active");
+    $(".header__nav-list").toggleClass("active");
+  });
 });
